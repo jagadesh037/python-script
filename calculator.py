621d099c99746739ab1dd6063bc738b86e875a1b
jagadesh037 <66157248+jagadesh037@users.noreply.github.com>
2020-06-03 20:42:26 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,19 +4,28 @@ import sys
  
 # Function to add two numbers
 def add(num1, num2):
-    return num1 + num2
+    output = num1 + num2
+    file_store(output)
  
 # Function to subtract two numbers
 def subtract(num1, num2):
-    return num1 - num2
+    output = num1 - num2
+    file_store(output)
  
 # Function to multiply two numbers
 def multiply(num1, num2):
-    return num1 * num2
+    output = num1 * num2
+    file_store(output)
  
 # Function to divide two numbers
 def divide(num1, num2):
-    return num1 / num2
+    output = num1 / num2
+    file_store(output)
+   
+def file_store(output):
+    file = open("sample.txt","w")
+    file.write(output)
+    file.close()
  
 print("Please select operation -\n" \
         "1. Add\n" \
