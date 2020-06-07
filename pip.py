8781106574483b435d6043b240013e2a42c35962
jagadesh037 <66157248+jagadesh037@users.noreply.github.com>
2020-06-07 13:03:14 +0530

    Update pip.py

--- pip.py
+++ pip.py
@@ -1,1 +1,14 @@
-pip install tomcatmanager
+#Deploy vpn pod
+#Verify the state of pod
+#Modify the vpn pod
+#Delete the vpn pod
+#verify the vpn pod deleted properly
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time 
+from selenium.webdriver.support.ui import Select
+driver = webdriver.Chrome()
+
+#Enter into webpage
+driver.get("https://connect.netsurion.com/login")
