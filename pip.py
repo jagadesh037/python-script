635d334d11c435743d77320951a74c52ddecf857
jagadesh037 <66157248+jagadesh037@users.noreply.github.com>
2020-06-10 12:20:35 +0530

    Update pip.py

--- pip.py
+++ pip.py
@@ -1,14 +1,2 @@
-#Deploy vpn pod
-#Verify the state of pod
-#Modify the vpn pod
-#Delete the vpn pod
-#verify the vpn pod deleted properly
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time 
-from selenium.webdriver.support.ui import Select
-driver = webdriver.Chrome()
-
-#Enter into webpage
-driver.get("https://connect.netsurion.com/login")
+import webbrowser
+webbrowser.open("http://localhost:8081/app1/",new=2)
